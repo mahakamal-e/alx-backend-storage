ec01e93bd1ae8e830e5bf46c13da5e62b6981580
mahakamal-e <mahakamal09@gmail.com>
2024-07-11 03:06:41 +0000

    task 5

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- Script that creates a trigger to reset valid_email when email is updated
+DELIMITER //
+
+CREATE TRIGGER reset_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END //
+
+DELIMITER ;
