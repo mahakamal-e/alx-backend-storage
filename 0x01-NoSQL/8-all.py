4629180060c800081aa4fcd31cca384e05f21d14
mahakamal-e <mahakamal09@gmail.com>
2024-07-16 19:51:15 +0000

    edit

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,6 +2,6 @@
 """ Define function that lists all document"""
 
 
-def ist_all(mongo_collection):
+def list_all(mongo_collection):
     """Return an empty list if no document in the collection"""
     return mongo_collection.find()
