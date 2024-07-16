cabde6a868e6b217d7866c24f7d1b24135210817
mahakamal-e <mahakamal09@gmail.com>
2024-07-16 20:00:57 +0000

    edit task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,5 +4,5 @@
 
 def insert_school(mongo_collection, **kwargs):
     """inserts a new document in a collection based on kwargs"""
-    new_items = mango_collection.insert_one(kwargs)
+    new_items = mongo_collection.insert_one(kwargs)
     return new_items.inserted_id
