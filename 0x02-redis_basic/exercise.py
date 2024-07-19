32f05ad2cbe868e61058e42365901dbe1d123b9f
mahakamal-e <mahakamal09@gmail.com>
2024-07-19 21:42:33 +0000

    edit

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,7 +8,7 @@ from typing import Union
 class Cache:
     def __init__(self):
         """Initialize the Cache instance with a Redis client and flush the database."""
-        self._redis = redis.Redis(host='localhost', port=6379, db=0)
+        self._redis = redis.Redis()
         self._redis.flushdb()
     
     def store(self, data: Union[str, bytes, int, float]) -> str:
