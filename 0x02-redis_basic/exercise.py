fc346b80f7177dddea2444dea49d540600ba5ec2
mahakamal-e <mahakamal09@gmail.com>
2024-07-19 22:17:57 +0000

    edit

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ Create a Cache class. """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -24,3 +24,47 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+     def get(self, key: str,
+             fn: Optional[Callable[[bytes], any]] = None) -> Optional[any]:
+        """
+        Retrieve the data from Redis by key and apply,
+        the optional conversion function.
+
+        Args:
+            key: The key for the data in Redis.
+            fn: An optional callable that takes bytes and returns the desired format.
+
+        Returns:
+            The retrieved data in the desired format, or None if the key does not exist.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve the data as a string.
+
+        Args:
+            key: The key for the data in Redis.
+
+        Returns:
+            The retrieved data as a string, or None if the key does not exist.
+        """
+        return self.get(key, lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve the data as an integer.
+
+        Args:
+            key: The key for the data in Redis.
+
+        Returns:
+            The retrieved data as an integer, or None if the key does not exist.
+        """
+        return self.get(key, lambda d: int(d))
